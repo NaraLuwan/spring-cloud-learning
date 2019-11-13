b2cc95a115f4391dc5107c81773be3d5f4bde9cc
NaraLuwan <hqcadmin@163.com>
2019-11-13 14:47:09 +0800

    优化配置

--- spring-cloud-gateway-zuul/src/main/java/com/github/luwan/spring/cloud/gateway/zuul/GatewayZuulStarter.java
+++ spring-cloud-gateway-zuul/src/main/java/com/github/luwan/spring/cloud/gateway/zuul/GatewayZuulStarter.java
@@ -1,9 +1,7 @@
 package com.github.luwan.spring.cloud.gateway.zuul;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,12 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class GatewayZuulStarter {
 
     public static void main(String[] args) {
-        // 设置app启动参数
-        SpringApplication application = new SpringApplicationBuilder().sources(GatewayZuulStarter.class)
-                .addCommandLineProperties(true).registerShutdownHook(true).bannerMode(Banner.Mode.OFF)
-                .build(args);
-        application.setWebEnvironment(true);
-        application.run(args);
+        SpringApplication.run(GatewayZuulStarter.class, args);
     }
 
 }
